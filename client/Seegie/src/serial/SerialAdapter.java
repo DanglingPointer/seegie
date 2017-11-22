df52820c6ab8a362646115982f35b1dda04f5d63
DanglingPointer <mikail.vasilyev@gmail.com>
2017-11-22 14:42:01 +0100

    Fixed GPL license, Apache was illegal

--- client/Seegie/src/serial/SerialAdapter.java
+++ client/Seegie/src/serial/SerialAdapter.java
@@ -1,17 +1,18 @@
 /*
- *    Copyright 2017 Mikhail Vasilyev
+ *     Copyright (C) 2017  Mikhail Vasilyev
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package serial;
@@ -29,6 +30,8 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.fazecast.jSerialComm.SerialPort.TIMEOUT_WRITE_BLOCKING;
 
 public class SerialAdapter implements DataInEndpoint, CmdOutEndpoint
 {
@@ -155,6 +158,11 @@ public class SerialAdapter implements DataInEndpoint, CmdOutEndpoint
     @Override
     public void open() {
         m_port.setBaudRate(115200);
+        m_port.setNumDataBits(8);
+        m_port.setParity(SerialPort.NO_PARITY);
+        m_port.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        m_port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING | SerialPort.TIMEOUT_WRITE_BLOCKING, 500, 500);
+
         m_port.openPort();
         System.out.println("Serial port opened");
     }
